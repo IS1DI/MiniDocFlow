1f991622851590bab455aa6b15049bcc65f51521
IS1DI <slavaivankov236@gmail.com>
2023-03-16 16:21:47 +0300

    Spring config

--- src/main/java/MiniDFlow/Starter.java
+++ src/main/java/MiniDFlow/Starter.java
@@ -4,6 +4,7 @@ package MiniDFlow;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 public class Starter {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext(
