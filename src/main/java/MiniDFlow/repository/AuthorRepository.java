bea23bdfc2e958ae1462a4773754d9df79d6965f
IS1DI <slavaivankov236@gmail.com>
2023-03-17 12:58:59 +0300

    some fixes

--- src/main/java/MiniDFlow/repository/AuthorRepository.java
+++ src/main/java/MiniDFlow/repository/AuthorRepository.java
@@ -1,8 +1,11 @@
 package MiniDFlow.repository;
 
 import MiniDFlow.repository.entity.Author;
+import com.sun.istack.NotNull;
 import jakarta.persistence.TypedQuery;
+import javassist.bytecode.DuplicateMemberException;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -24,16 +27,21 @@ public class AuthorRepository {
      *
      * @param author Должны быть заполнены все поля
      *               !!!КРОМЕ ID!!!
+     * @throws DuplicateMemberException если такой login существует в бд
      */
-    public void create(Author author) {
-        session.persist(author);
+    public void create(Author author) throws DuplicateMemberException {
+        try {
+            session.persist(author);
+        }catch (Exception ex){
+            throw new DuplicateMemberException("author с таким login существует");
+        }
     }
 
     /**
      * Получение информации о пользователе по id
      *
      * @param id: идентификатор пользователя
-     * @return объект Author, соответствующий заданному идентификатору
+     * @return объект Author, соответствующий заданному идентификатору ИЛИ null, если по такому id нет сущности
      */
     public Author getById(Integer id) {
         return session.find(Author.class, id);
@@ -46,7 +54,7 @@ public class AuthorRepository {
      * @return объект Author, соответствующий заданному имени пользователя
      */
     public Optional<Author> getByUsername(String username) {
-        String hql = "FROM Author WHERE Author.userName = :username";
+        String hql = "FROM Author WHERE userName = :username";
         TypedQuery<Author> query = session.createQuery(hql, Author.class);
         query.setParameter("username", username);
         List<Author> results = query.getResultList();
@@ -67,8 +75,8 @@ public class AuthorRepository {
     public void updateUsernameById(Integer id, String newUsername) throws NoSuchElementException {
         Author author = getById(id);
         if (author != null) {
-            String hql = "UPDATE Author SET Author.userName = :newUsername WHERE id = :id";
-            TypedQuery<Author> query = session.createQuery(hql, Author.class);
+            String hql = "UPDATE Author SET userName = :newUsername WHERE id = :id";
+            Query query = session.createQuery(hql);
             query.setParameter("newUsername", newUsername);
             query.setParameter("id", id);
             query.executeUpdate();
@@ -83,7 +91,7 @@ public class AuthorRepository {
      *
      * @param author: объект Author с новым именем пользователя и существующим идентификатором
      */
-    public void updateUsernameByEntity(Author author, String newUsername) {
+    public void updateUsernameByEntity(@NotNull Author author, String newUsername) {
         author.setUserName(newUsername);
         session.merge(author);
     }
