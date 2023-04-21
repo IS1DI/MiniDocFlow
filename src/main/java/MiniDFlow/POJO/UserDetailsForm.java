8ce7acb9404894bda2585b1f7ff8d8c283afb354
IS1DI <slavaivankov236@gmail.com>
2023-04-21 20:11:17 +0300

    help class

---
+++ src/main/java/MiniDFlow/POJO/UserDetailsForm.java
@@ -0,0 +1,64 @@
+package MiniDFlow.POJO;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsForm implements UserDetails {
+    private String username;
+    private String password;
+
+    public UserDetailsForm() {
+    }
+
+    public UserDetailsForm(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    public UserDetailsForm(UserRegForm user){
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER")) ;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
